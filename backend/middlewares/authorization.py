2d2e3f3f80a748ba71bc95ebb2a1133a496fb8d8
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-11 16:12:55 +0200

    feat: implement authorization middleware for the future use of the supabase service

---
+++ backend/middlewares/authorization.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+from supabase import create_client, Client
+
+from backend.config import config
+from backend.middlewares.schemas import ResponseWrapper
+
+supabase: Client = create_client(config.supabase_url, config.supabase_key)
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if request.method == "OPTIONS":
+            response = await call_next(request)
+            return response
+
+        try:
+            authorization: Optional[str] = request.headers.get("Authorization")
+            if not authorization or not authorization.startswith("Bearer "):
+                raise HTTPException(status_code=401, detail="Missing or invalid Authorization header")
+
+            token = authorization.split(" ")[1]
+
+            user_data = supabase.auth.get_user(token)
+            if user_data.user:
+                request.state.user = user_data.user
+                response = await call_next(request)
+                return response
+            else:
+                raise HTTPException(status_code=401, detail="Invalid token")
+        except Exception as exception:
+            response = ResponseWrapper(data=None, success=False, error=str(exception))
+            if isinstance(exception, HTTPException):
+                return JSONResponse(content=response.model_dump(), status_code=exception.status_code)
+            return JSONResponse(content=response.model_dump(), status_code=500)
