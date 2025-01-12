9ef854c7a4cedfeccb5280368212b8afcb3f77db
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-12 20:17:34 +0200

    refactor: improved channel sorting logic and add type hints

--- backend/services/fetch.py
+++ backend/services/fetch.py
@@ -1,30 +1,44 @@
+from disnake import Role, Member, VoiceChannel, TextChannel, CategoryChannel, Guild
+
 from backend.common.variables import variables
 from backend.services.bot import bot
 
 
-async def fetch_guild():
+async def fetch_guild() -> Guild:
     return await bot.fetch_guild(variables.GUILD_ID)
 
 
-async def fetch_channels():
+async def fetch_channels() -> list[VoiceChannel | TextChannel | CategoryChannel]:
     guild = await fetch_guild()
-    channels = await guild.fetch_channels()
+    channels_sequence = await guild.fetch_channels()
+    channels = list(channels_sequence)
     channels.sort(key=lambda c: (c.position, c.id))
     return channels
 
 
-async def fetch_channel(channel_id):
+async def fetch_channel(channel_id) -> VoiceChannel | TextChannel | CategoryChannel:
     guild = await fetch_guild()
     channel = await guild.fetch_channel(channel_id)
     return channel
 
 
-async def fetch_users():
+async def fetch_users() -> list[Member]:
     guild = await fetch_guild()
     members = await guild.fetch_members().flatten()
     return members
 
+async def fetch_roles() -> list[Role]:
+    guild = await fetch_guild()
+    roles = await guild.fetch_roles()
+    return roles
 
-async def fetch_guild_default_role():
+
+async def fetch_role(role_id: int) -> Role:
+    guild = await fetch_guild()
+    role = await guild.fetch_role(role_id)
+    return role
+
+
+async def fetch_guild_default_role() -> Role:
     guild = await fetch_guild()
     return guild.default_role
