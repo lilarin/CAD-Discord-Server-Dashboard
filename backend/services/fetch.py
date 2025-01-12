7e7b17a33293916fb576bd59eefd2db3bb0b1a48
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-12 16:03:45 +0200

    refactor: improve method to fetch channels

--- backend/services/fetch.py
+++ backend/services/fetch.py
@@ -5,15 +5,25 @@ from backend.services.bot import bot
 async def fetch_guild():
     return await bot.fetch_guild(variables.GUILD_ID)
 
+
 async def fetch_channels():
     guild = await fetch_guild()
     channels = await guild.fetch_channels()
-    return sorted(channels, key=lambda ch: ch.position)
+    channels.sort(key=lambda c: (c.position, c.id))
+    return channels
+
+
+async def fetch_channel(channel_id):
+    guild = await fetch_guild()
+    channel = await guild.fetch_channel(channel_id)
+    return channel
+
 
 async def fetch_users():
     guild = await fetch_guild()
     members = await guild.fetch_members().flatten()
     return members
+
 
 async def fetch_guild_default_role():
     guild = await fetch_guild()
