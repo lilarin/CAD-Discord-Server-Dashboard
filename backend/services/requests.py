e0a1a868e670591c3b49a2d61d01addf36d015f9
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-12 16:06:54 +0200

    feat: implement own method to update channel order bc discords sdk method is incorrect

---
+++ backend/services/requests.py
@@ -0,0 +1,25 @@
+import disnake
+from typing import List
+
+import aiohttp
+
+from backend.common.variables import variables
+from backend.config import config
+from backend.schemas import BaseChannel
+
+
+async def update_channel_order(payload: List[BaseChannel]):
+    try:
+        url = f"https://discord.com/api/v10/guilds/{variables.GUILD_ID}/channels"
+        headers = {
+            "Authorization": f"Bot {config.discord_bot_token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        serializable_response = [channel.model_dump() for channel in payload]
+        async with aiohttp.ClientSession() as session:
+            async with session.patch(url, headers=headers, json=serializable_response) as response:
+                if not response.ok:
+                    raise disnake.errors.HTTPException(response=response, message=str(response))
+    except Exception:
+        raise
