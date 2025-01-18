71df458ca8241fb8666d13a1a1cf20e6634e67c5
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-18 07:43:19 +0200

    feat: implement new spinner to separate logic and make frontend prettier

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,7 +1,16 @@
-export const LoadingSpinner = () => {
+export const ChannelLoadingSpinner = () => {
   return (
-      <div className="fixed top-1/2  flex items-center justify-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-500"></div>
-      </div>
+    <div className="inline-flex items-center justify-center ">
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-500"></div>
+    </div>
   );
 };
+
+
+export const ComponentLoadingSpinner = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-500"></div>
+    </div>
+  );
+}
