5f773f6f3b356ff65c15903af4b1ed639b63480d
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-19 21:24:15 +0200

    feat: improve categories dragging

--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -14,9 +14,11 @@ import RenameIcon from '@/assets/icons/rename.svg';
 import EditIcon from '@/assets/icons/edit.svg';
 import DeleteIcon from '@/assets/icons/delete.svg';
 import DeleteHoveredIcon from '@/assets/icons/delete_hover.svg';
-import PlusIcon from '@/assets/icons/plus.svg';
+import CreateCategory from '@/assets/icons/create_category.svg';
+import CreateChannel from '@/assets/icons/create_channel.svg';
 import TextIcon from '@/assets/icons/text.svg';
 import VoiceIcon from '@/assets/icons/voice.svg';
+import ReorderIcon from '@/assets/icons/reorder.svg';
 import {
   DndContext,
   closestCenter,
@@ -63,26 +65,17 @@ function DraggableCategory({
         onClick={() => handleCategoryClick(category.id)}
       >
         <span className="cursor-grab mr-2" {...attributes} {...listeners}>
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="5" r="1" />
-            <circle cx="12" cy="12" r="1" />
-            <circle cx="12" cy="19" r="1" />
-          </svg>
+          <img
+              src={ReorderIcon}
+              alt="Перемістити"
+              className="w-5 h-5 cursor-grab"
+          />
         </span>
         <span>{category.name.charAt(0).toUpperCase() + category.name.slice(1)}</span>
       </div>
       <div className="flex justify-end space-x-2">
         <div className="relative group">
-          <img
+        <img
             src={RenameIcon}
             alt="Перейменувати"
             className="w-5 h-5 cursor-pointer filter group-hover:brightness-200 transition-all duration-200"
@@ -130,29 +123,20 @@ function DraggableChannel({
     <li
       ref={setNodeRef}
       style={style}
-      className="p-1 bg-[#2f3136] hover:bg-[#2a2c31] rounded ml-4 flex items-center justify-between pr-2"
+      className="p-1 bg-[#2f3136] hover:bg-[#2a2c31] rounded ml-4 flex items-center justify-between"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="flex justify-start w-4/5 items-center">
         <span className="cursor-grab mr-2" {...attributes} {...listeners}>
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="5" r="1" />
-            <circle cx="12" cy="12" r="1" />
-            <circle cx="12" cy="19" r="1" />
-          </svg>
+          <img
+              src={ReorderIcon}
+              alt="Перемістити"
+              className="w-5 h-5 cursor-grab"
+          />
         </span>
         {channel.type === 'text' && (
-          <img src={TextIcon} alt="Текстовий канал" className="w-4 h-4 mr-1" />
+            <img src={TextIcon} alt="Текстовий канал" className="w-4 h-4 mr-1"/>
         )}
         {channel.type === 'voice' && (
           <img src={VoiceIcon} alt="Голосовий канал" className="w-4 h-4 mr-1" />
@@ -191,6 +175,7 @@ export default function Categories() {
   const [hoveredCategoryId, setHoveredCategoryId] = useState<number | null>(null);
   const [hoveredChannelId, setHoveredChannelId] = useState<number | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isDraggingCategory, setIsDraggingCategory] = useState(false);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -207,11 +192,14 @@ export default function Categories() {
       }
     };
 
-    fetchCategories();
+    fetchCategories().then(r => {});
   }, []);
 
   const handleCategoryClick = useCallback(
     async (categoryId: number) => {
+      if (isDraggingCategory) {
+        return;
+      }
       if (openCategoryId === categoryId) {
         setOpenCategoryId(null);
         setChannels([]);
@@ -232,7 +220,7 @@ export default function Categories() {
         setIsChannelsLoading(false);
       }
     },
-    [openCategoryId]
+    [openCategoryId, isDraggingCategory]
   );
 
   const handleDeleteCategory = useCallback(
@@ -291,8 +279,15 @@ export default function Categories() {
     })
   );
 
+  const handleDragStart = useCallback(({ active }) => {
+    if (categories.find((cat) => cat.id === active.id)) {
+      setIsDraggingCategory(true);
+    }
+  }, [categories]);
+
   const handleDragEnd = useCallback(
     async ({ active, over }) => {
+      setIsDraggingCategory(false);
       if (!over || active.id === over.id) {
         return;
       }
@@ -349,103 +344,101 @@ export default function Categories() {
   const voiceChannels = channels.filter((channel) => channel.type === 'voice');
 
   return (
-    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd} sensors={sensors}>
+    <DndContext collisionDetection={closestCenter} onDragStart={handleDragStart} onDragEnd={handleDragEnd} sensors={sensors}>
       <div className="p-6 flex justify-left">
-        {isLoading && <ComponentLoadingSpinner />}
-        <div className="w-2/3">
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Пошук за назвою категорії..."
-              className="w-full p-2 rounded bg-[#292B2F] text-white focus:outline-none"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-          </div>
-
-          <SortableContext items={filteredCategories.map((cat) => cat.id)}>
-            <div className="mt-4 space-y-2">
-              {filteredCategories.length === 0 && !isLoading && (
-                <div className="flex items-center justify-left text-gray-400">Категорій немає</div>
-              )}
-              {filteredCategories.map((category) => (
-                <div key={category.id}>
-                  <DraggableCategory
-                    category={category}
-                    handleCategoryClick={handleCategoryClick}
-                    handleDeleteCategory={handleDeleteCategory}
-                    openCategoryId={openCategoryId}
-                    hoveredCategoryId={hoveredCategoryId}
-                    setHoveredCategoryId={setHoveredCategoryId}
-                  />
-                  {openCategoryId === category.id && (
-                    <div
-                      ref={channelsRef}
-                      className="mt-2 overflow-hidden transition-max-height duration-300 ease-in-out"
-                    >
-                      {isChannelsLoading ? (
-                        <div className="flex justify-center items-center p-2">
-                          <ChannelLoadingSpinner />
-                        </div>
-                      ) : (
-                        <>
-                          {textChannels.length > 0 && (
-                            <SortableContext items={textChannels.map((channel) => channel.id)}>
-                              <ul className="space-y-1">
-                                {textChannels.map((channel) => (
-                                  <DraggableChannel
-                                    key={channel.id}
-                                    channel={channel}
-                                    handleDeleteChannel={handleDeleteChannelFromCategory}
-                                    hoveredChannelId={hoveredChannelId}
-                                    setHoveredChannelId={setHoveredChannelId}
-                                  />
-                                ))}
-                              </ul>
-                            </SortableContext>
-                          )}
-                          {voiceChannels.length > 0 && (
-                            <SortableContext items={voiceChannels.map((channel) => channel.id)}>
-                              <ul className="space-y-1 mt-1">
-                                {voiceChannels.map((channel) => (
-                                  <DraggableChannel
-                                    key={channel.id}
-                                    channel={channel}
-                                    handleDeleteChannel={handleDeleteChannelFromCategory}
-                                    hoveredChannelId={hoveredChannelId}
-                                    setHoveredChannelId={setHoveredChannelId}
-                                  />
-                                ))}
-                              </ul>
-                            </SortableContext>
-                          )}
-                          {textChannels.length === 0 && voiceChannels.length === 0 && (
-                            <div className="flex items-center justify-left ml-4 text-gray-400">
-                              Каналів немає
-                            </div>
-                          )}
-                          <div className="flex items-center justify-center p-1 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer mt-2 ml-4">
-                            <img src={PlusIcon} alt="Створити канал" className="w-4 h-4" />
-                          </div>
-                        </>
-                      )}
-                    </div>
-                  )}
-                </div>
-              ))}
-              {!isLoading && filteredCategories.length > 0 && (
-                <div className="w-full flex items-center justify-center p-2 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer">
-                  <img src={PlusIcon} alt="Створити категорію" className="w-5 h-5" />
-                </div>
-              )}
-              {!isLoading && filteredCategories.length === 0 && (
-                <div className="w-full flex items-center justify-center p-2 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer">
-                  <img src={PlusIcon} alt="Створити категорію" className="w-5 h-5" />
-                </div>
-              )}
+        {isLoading ? (
+            <ComponentLoadingSpinner/>
+        ) : (
+          <div className="w-2/3">
+            <div className="mb-4">
+              <input
+                type="text"
+                placeholder="Пошук за назвою категорії..."
+                className="w-full p-2 rounded bg-[#292B2F] text-white focus:outline-none"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
             </div>
-          </SortableContext>
-        </div>
+
+            <SortableContext items={filteredCategories.map((cat) => cat.id)}>
+              <div className="mt-4 space-y-2">
+                {filteredCategories.length === 0 && !isLoading && (
+                  <div className="flex items-center justify-left text-gray-400">Категорій немає</div>
+                )}
+                {filteredCategories.map((category) => (
+                  <div key={category.id}>
+                    <DraggableCategory
+                      category={category}
+                      handleCategoryClick={handleCategoryClick}
+                      handleDeleteCategory={handleDeleteCategory}
+                      openCategoryId={openCategoryId}
+                      hoveredCategoryId={hoveredCategoryId}
+                      setHoveredCategoryId={setHoveredCategoryId}
+                    />
+                    {openCategoryId === category.id && !isDraggingCategory && (
+                      <div
+                        ref={channelsRef}
+                        className="mt-2 overflow-hidden transition-max-height duration-300 ease-in-out"
+                      >
+                        {isChannelsLoading ? (
+                          <div className="flex justify-center items-center p-2">
+                            <ChannelLoadingSpinner />
+                          </div>
+                        ) : (
+                          <>
+                            {textChannels.length > 0 && (
+                              <SortableContext items={textChannels.map((channel) => channel.id)}>
+                                <ul className="space-y-1">
+                                  {textChannels.map((channel) => (
+                                    <DraggableChannel
+                                      key={channel.id}
+                                      channel={channel}
+                                      handleDeleteChannel={handleDeleteChannelFromCategory}
+                                      hoveredChannelId={hoveredChannelId}
+                                      setHoveredChannelId={setHoveredChannelId}
+                                    />
+                                  ))}
+                                </ul>
+                              </SortableContext>
+                            )}
+                            {voiceChannels.length > 0 && (
+                              <SortableContext items={voiceChannels.map((channel) => channel.id)}>
+                                <ul className="space-y-1 mt-1">
+                                  {voiceChannels.map((channel) => (
+                                    <DraggableChannel
+                                      key={channel.id}
+                                      channel={channel}
+                                      handleDeleteChannel={handleDeleteChannelFromCategory}
+                                      hoveredChannelId={hoveredChannelId}
+                                      setHoveredChannelId={setHoveredChannelId}
+                                    />
+                                  ))}
+                                </ul>
+                              </SortableContext>
+                            )}
+                            {textChannels.length === 0 && voiceChannels.length === 0 && (
+                              <div className="flex items-center justify-left ml-4 text-gray-400">
+                                Каналів немає
+                              </div>
+                            )}
+                            <div className="flex items-center justify-center p-1 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer mt-2 ml-4">
+                              <img src={CreateChannel} alt="Створити канал" className="w-4 h-4" />
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                ))}
+                {!isLoading && filteredCategories.length > 0 && (
+                  <div className="w-full flex items-center justify-center p-2 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer">
+                    <img src={CreateCategory} alt="Створити категорію" className="w-5 h-5" />
+                  </div>
+                )}
+              </div>
+            </SortableContext>
+          </div>
+        )}
       </div>
     </DndContext>
   );
