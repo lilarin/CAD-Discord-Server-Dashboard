b7cedc886aa68286ef9d71332021bf62519e272e
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-21 22:30:11 +0200

    feat: improve logic to obtain only "default" roles

--- backend/api/v1/roles.py
+++ backend/api/v1/roles.py
@@ -1,6 +1,7 @@
 import disnake
 from fastapi import APIRouter, HTTPException
 
+from backend.common.variables import variables
 from backend.middlewares.uniform_response import uniform_response_middleware
 from backend.schemas import Role
 from backend.services.fetch import fetch_roles, fetch_guild_default_role
@@ -14,11 +15,20 @@ async def get_roles():
     try:
         default_role = await fetch_guild_default_role()
         roles = [
-            Role(id=str(role.id), name=role.name)
+            Role(
+                id=str(role.id),
+                name=role.name
+            )
             for role in await fetch_roles()
-            if role != default_role
+            if role != default_role and role.id != variables.ADMINISTRATOR_ROLE_ID
         ]
-        roles.sort(key=lambda user: user.name)
+        roles = sorted(roles, key=lambda role: (
+            0 if int(role.id) == variables.TEACHER_ROLE_ID else
+            1 if int(role.id) == variables.STUDENT_ROLE_ID else
+            2,
+            role.name.lower()
+        ))
+
         return roles
     except disnake.errors.HTTPException as exception:
         raise HTTPException(status_code=exception.status, detail=str(exception.text))
