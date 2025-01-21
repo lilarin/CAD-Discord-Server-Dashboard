6a7a44c5075542e695f67feb195aa02c87cd0c33
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-21 22:30:41 +0200

    feat: implement new formatting methods

--- backend/services/format.py
+++ backend/services/format.py
@@ -1,8 +1,8 @@
 import disnake
 from disnake import CategoryChannel, VoiceChannel, TextChannel
 
-from backend.schemas import Channel, BaseChannel, Category
-from backend.services.fetch import fetch_channels
+from backend.schemas import Channel, BaseChannel, Category, Role
+from backend.services.fetch import fetch_channels, fetch_roles_with_access
 
 
 async def format_categories_response() -> list[Category]:
@@ -30,6 +30,26 @@ async def format_channels_by_category_response(category: CategoryChannel) -> lis
     ]
     channels.sort(key=lambda channel: (channel.type != 'text', channel.position))
     return channels
+
+
+async def format_roles_with_access_response(category: CategoryChannel) -> list[Role]:
+    return [
+        Role(
+            id=str(target.id),
+            name=target.name
+        )
+        for target in await fetch_roles_with_access(category)
+    ]
+
+
+async def format_roles_with_access_by_roles(roles) -> list[Role]:
+    return [
+        Role(
+            id=str(target.id),
+            name=target.name
+        )
+        for target in roles
+    ]
 
 
 async def format_base_channel_response(channel: TextChannel | VoiceChannel, index: int) -> BaseChannel:
