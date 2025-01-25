9891e4a7b5063f477aa357296aa8384b0b5798ef
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-26 01:05:02 +0200

    feat: implement endpoints to work with users

--- backend/api/v1/users.py
+++ backend/api/v1/users.py
@@ -1,9 +1,11 @@
 import disnake
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Body
 
 from backend.middlewares.uniform_response import uniform_response_middleware
 from backend.schemas import User
-from backend.services.fetch import fetch_users, fetch_role
+from backend.services.fetch import fetch_roles_by_ids, fetch_user, fetch_user_roles, fetch_guild_default_role
+from backend.services.format import format_users_response, format_roles_response
+from backend.services.utils import kick_target_user, rename_target_user
 
 router = APIRouter()
 
@@ -12,36 +14,73 @@ router = APIRouter()
 @uniform_response_middleware
 async def get_users():
     try:
-        users = [
-            User(
-                id=str(member.id),
-                name=member.display_name
-            )
-            for member in await fetch_users()
-        ]
-        users.sort(key=lambda user: user.name)
-        return users
+        return await format_users_response()
     except disnake.errors.HTTPException as exception:
         raise HTTPException(status_code=exception.status, detail=str(exception.text))
     except Exception as exception:
         raise HTTPException(status_code=500, detail=str(exception))
 
 
-@router.get("/users/roles/{role_id}", response_model=list[User])
+@router.patch("/users/{user_id}/rename/{name}", response_model=list[User])
 @uniform_response_middleware
-async def get_users_by_role(role_id: int):
+async def rename_user(user_id: int, name: str):
     try:
-        role = await fetch_role(role_id)
-        users = [
-            User(
-                id=str(member.id),
-                name=member.display_name
-            )
-            for member in await fetch_users()
-            if role in member.roles
-        ]
-        users.sort(key=lambda user: user.name)
-        return users
+        user = await fetch_user(user_id)
+        await rename_target_user(user, name)
+        return await format_users_response()
+    except disnake.errors.HTTPException as exception:
+        raise HTTPException(status_code=exception.status, detail=str(exception.text))
+    except Exception as exception:
+        raise HTTPException(status_code=500, detail=str(exception))
+
+
+@router.get("/users/{user_id}", response_model=list[User])
+@uniform_response_middleware
+async def get_user_roles(user_id: int):
+    try:
+        user = await fetch_user(user_id)
+        roles = await fetch_user_roles(user)
+        return await format_roles_response(roles)
+    except disnake.errors.HTTPException as exception:
+        raise HTTPException(status_code=exception.status, detail=str(exception.text))
+    except Exception as exception:
+        raise HTTPException(status_code=500, detail=str(exception))
+
+
+@router.put("/users/{user_id}", response_model=list[User])
+@uniform_response_middleware
+async def change_user_roles(user_id: int, roles: list[str] = Body(...)):
+    try:
+        try:
+            new_roles = await fetch_roles_by_ids(roles)
+        except disnake.errors.HTTPException:
+            raise ValueError("Incorrect object in roles")
+
+        user = await fetch_user(user_id)
+        actual_roles = await fetch_user_roles(user)
+
+        default_role = await fetch_guild_default_role()
+        for role in actual_roles:
+            if role not in new_roles and role != default_role:
+                await user.remove_roles(role)
+
+        for role in new_roles:
+            await user.add_roles(role)
+
+        return await format_users_response()
+    except disnake.errors.HTTPException as exception:
+        raise HTTPException(status_code=exception.status, detail=str(exception.text))
+    except Exception as exception:
+        raise HTTPException(status_code=500, detail=str(exception))
+
+
+@router.delete("/users/{user_id}", response_model=list[User])
+@uniform_response_middleware
+async def kick_user(user_id: int):
+    try:
+        user = await fetch_user(user_id)
+        await kick_target_user(user)
+        return await format_users_response()
     except disnake.errors.HTTPException as exception:
         raise HTTPException(status_code=exception.status, detail=str(exception.text))
     except Exception as exception:
