c09a34350a426a0840f1dd6212c635102059e8cb
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-25 14:44:14 +0200

    feat: optimize api and separate types from it

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,333 +1,125 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import {Category, Channel, Role} from "@/lib/types.ts";
 
 if (!import.meta.env.VITE_API_URL) {
-  throw new Error('Missing environment variable: VITE_API_URL');
+    throw new Error('Missing environment variable: VITE_API_URL');
 }
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 export class ApiError extends Error {
-  code: number
+    code: number;
 
-  constructor(message: string, code: number) {
-    super(message);
-    this.code = code;
-    this.name = 'ApiError';
-  }
+    constructor(message: string, code: number) {
+        super(message);
+        this.code = code;
+        this.name = 'ApiError';
+    }
 }
 
 export class ClientError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'ClientError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.name = 'ClientError';
+    }
 }
 
-export interface Category {
-  id: number;
-  name: string;
-}
-
-export interface Role {
-  id: number;
-  name: string;
-}
-
-export interface Channel {
-  id: number;
-  position: number;
-  name: string;
-  type: string
-}
-
-export interface User {
-  id: number;
-  position: number;
-  name: string;
-  type: string
-}
 
 interface ApiResponse<T> {
-  data: T;
-  success: boolean;
-  error: string | null;
+    data: T;
+    success: boolean;
+    error: string | null;
 }
 
-export async function getCategories(): Promise<Category[]> {
-  try {
-    const response = await api.get<ApiResponse<Category[]>>(
-        '/api/v1/categories'
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
+async function handleRequest<T>(apiCall: Promise<AxiosResponse<ApiResponse<T>>>): Promise<T> {
+    try {
+        const response = await apiCall;
+        return response.data.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError;
+            const statusCode = axiosError.response?.status || 500;
+            throw new ApiError(axiosError.message, statusCode);
+        } else if (error instanceof Error) {
+            throw new ClientError(error.message);
+        } else {
+            throw new ClientError(`Something went wrong: ${String(error)}`);
+        }
     }
-  }
+}
+
+
+export async function getCategories(): Promise<Category[]> {
+    return handleRequest(api.get<ApiResponse<Category[]>>('/api/v1/categories'));
 }
 
 export async function getChannels(categoryId: number): Promise<Channel[]> {
-  try {
-    const response = await api.get<ApiResponse<Channel[]>>(
-        `/api/v1/channels/${categoryId}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+    return handleRequest(api.get<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}`));
 }
 
 export async function deleteCategory(categoryId: number): Promise<Category[]> {
-  try {
-    const response = await api.delete<ApiResponse<Category[]>>(
-        `/api/v1/categories/${categoryId}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+    return handleRequest(api.delete<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}`));
 }
 
-
 export async function deleteChannel(channelId: number): Promise<Channel[]> {
-  try {
-    const response = await api.delete<ApiResponse<Channel[]>>(
-        `/api/v1/channels/${channelId}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+    return handleRequest(api.delete<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}`));
 }
 
 export async function updateCategoryPosition(categoryId: number, newPosition: number): Promise<Category[]> {
-  try {
-    const response = await api.patch<ApiResponse<Category[]>>(
-      `/api/v1/categories/${categoryId}/position/${newPosition}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+   return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}/position/${newPosition}`));
 }
 
 export async function updateChannelPosition(channelId: number, newPosition: number): Promise<Channel[]> {
-  try {
-    const response = await api.patch<ApiResponse<Channel[]>>(
-      `/api/v1/channels/${channelId}/position/${newPosition}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+    return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/position/${newPosition}`));
 }
 
 export async function createCategory(name: string): Promise<Category[]> {
-  try {
-    const response = await api.post<ApiResponse<Category[]>>(
-        `/api/v1/categories/${name}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+    return handleRequest(api.post<ApiResponse<Category[]>>(`/api/v1/categories/${name}`));
 }
 
 export async function createChannel(categoryId: number, name: string, channel_type: string): Promise<Channel[]> {
-  try {
-    const allowedChannelTypes = ["text", "voice"];
-    if (allowedChannelTypes.includes(channel_type)) {
-      const response = await api.post<ApiResponse<Channel[]>>(
-          `/api/v1/channels/${categoryId}/channel_name/${name}/${channel_type}`
-      );
-      return response.data.data;
-    }
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
+  const allowedChannelTypes = ["text", "voice"];
+  if (allowedChannelTypes.includes(channel_type)) {
+    return handleRequest(api.post<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}/channel_name/${name}/${channel_type}`));
   }
+  return Promise.reject(new ClientError('Invalid channel type'));
 }
 
 export async function renameChannel(channelId: number, name: string): Promise<Channel[]> {
-  try {
-    const response = await api.patch<ApiResponse<Channel[]>>(
-      `/api/v1/channels/${channelId}/rename/${name}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+  return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/rename/${name}`));
 }
 
 export async function renameCategory(channelId: number, name: string): Promise<Category[]> {
-  try {
-    const response = await api.patch<ApiResponse<Category[]>>(
-      `/api/v1/categories/${channelId}/rename/${name}`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+   return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${channelId}/rename/${name}`));
 }
 
 export async function getCategoryAccessRoles(categoryId: number): Promise<Role[]> {
-  try {
-    console.log(categoryId)
-    const response = await api.get<ApiResponse<Role[]>>(
-        `/api/v1/categories/${categoryId}/permissions`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`));
 }
 
 export async function editCategoryPermissions(categoryId: number, rolesWithAccess: number[]): Promise<Role[]> {
-  try {
-    const response = await api.put<ApiResponse<Role[]>>(
-      `/api/v1/categories/${categoryId}/permissions`,
-      rolesWithAccess
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(axiosError.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+  return handleRequest(api.put<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`, rolesWithAccess));
 }
 
-export async function getRoles(): Promise<Role[]> {
-  try {
-    const response = await api.get<ApiResponse<Role[]>>(
-        `/api/v1/roles`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+export async function getAllRoles(): Promise<Role[]> {
+  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles`));
 }
 
 export async function getEditableRoles(): Promise<Role[]> {
-  try {
-    const response = await api.get<ApiResponse<Role[]>>(
-        `/api/v1/roles/editable`
-    );
-    return response.data.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      const statusCode = axiosError.response?.status || 500;
-      throw new ApiError(error.message, statusCode);
-    } else if (error instanceof Error) {
-      throw new ClientError(error.message);
-    } else {
-      throw new ClientError(`Something went wrong: ${String(error)}`);
-    }
-  }
+  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles/editable`));
+}
+
+export async function renameRole(roleId: number, name: string): Promise<Role[]> {
+  return handleRequest(api.patch<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}/rename/${name}`));
+}
+
+export async function deleteRole(roleId: number): Promise<Role[]> {
+  return handleRequest(api.patch<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}`));
 }
---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,23 @@
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export interface Role {
+    id: number;
+    name: string;
+}
+
+export interface Channel {
+    id: number;
+    position: number;
+    name: string;
+    type: string
+}
+
+export interface User {
+    id: number;
+    position: number;
+    name: string;
+    type: string
+}
