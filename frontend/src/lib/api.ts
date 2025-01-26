aad5d88ffe42989db7493e157dbfac3147f77ded
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-26 02:27:10 +0200

    style: improve tabulation

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,133 +1,149 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, {AxiosError, AxiosResponse} from 'axios';
 import {Category, Channel, Role, User} from "@/lib/types.ts";
 
 if (!import.meta.env.VITE_API_URL) {
-    throw new Error('Missing environment variable: VITE_API_URL');
+	throw new Error('Missing environment variable: VITE_API_URL');
 }
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+	baseURL: API_BASE_URL,
+	headers: {
+		'Content-Type': 'application/json'
+	}
 });
 
 export class ApiError extends Error {
-    code: number;
+	code: number;
 
-    constructor(message: string, code: number) {
-        super(message);
-        this.code = code;
-        this.name = 'ApiError';
-    }
+	constructor(message: string, code: number) {
+		super(message);
+		this.code = code;
+		this.name = 'ApiError';
+	}
 }
 
 export class ClientError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'ClientError';
-    }
+	constructor(message: string) {
+		super(message);
+		this.name = 'ClientError';
+	}
 }
 
 
 interface ApiResponse<T> {
-    data: T;
-    success: boolean;
-    error: string | null;
+	data: T;
+	success: boolean;
+	error: string | null;
 }
 
 async function handleRequest<T>(apiCall: Promise<AxiosResponse<ApiResponse<T>>>): Promise<T> {
-    try {
-        const response = await apiCall;
-        return response.data.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            const axiosError = error as AxiosError;
-            const statusCode = axiosError.response?.status || 500;
-            throw new ApiError(axiosError.message, statusCode);
-        } else if (error instanceof Error) {
-            throw new ClientError(error.message);
-        } else {
-            throw new ClientError(`Something went wrong: ${String(error)}`);
-        }
-    }
+	try {
+		const response = await apiCall;
+		return response.data.data;
+	} catch (error) {
+		if (axios.isAxiosError(error)) {
+			const axiosError = error as AxiosError;
+			const statusCode = axiosError.response?.status || 500;
+			throw new ApiError(axiosError.message, statusCode);
+		} else if (error instanceof Error) {
+			throw new ClientError(error.message);
+		} else {
+			throw new ClientError(`Something went wrong: ${String(error)}`);
+		}
+	}
 }
 
 
 export async function getCategories(): Promise<Category[]> {
-    return handleRequest(api.get<ApiResponse<Category[]>>('/api/v1/categories'));
+	return handleRequest(api.get<ApiResponse<Category[]>>('/api/v1/categories'));
 }
 
 export async function getChannels(categoryId: number): Promise<Channel[]> {
-    return handleRequest(api.get<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}`));
+	return handleRequest(api.get<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}`));
 }
 
 export async function deleteCategory(categoryId: number): Promise<Category[]> {
-    return handleRequest(api.delete<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}`));
+	return handleRequest(api.delete<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}`));
 }
 
 export async function deleteChannel(channelId: number): Promise<Channel[]> {
-    return handleRequest(api.delete<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}`));
+	return handleRequest(api.delete<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}`));
 }
 
 export async function updateCategoryPosition(categoryId: number, newPosition: number): Promise<Category[]> {
-   return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}/position/${newPosition}`));
+	return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${categoryId}/position/${newPosition}`));
 }
 
 export async function updateChannelPosition(channelId: number, newPosition: number): Promise<Channel[]> {
-    return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/position/${newPosition}`));
+	return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/position/${newPosition}`));
 }
 
 export async function createCategory(name: string): Promise<Category[]> {
-    return handleRequest(api.post<ApiResponse<Category[]>>(`/api/v1/categories/${name}`));
+	return handleRequest(api.post<ApiResponse<Category[]>>(`/api/v1/categories/${name}`));
 }
 
 export async function createChannel(categoryId: number, name: string, channel_type: string): Promise<Channel[]> {
-  const allowedChannelTypes = ["text", "voice"];
-  if (allowedChannelTypes.includes(channel_type)) {
-    return handleRequest(api.post<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}/channel_name/${name}/${channel_type}`));
-  }
-  return Promise.reject(new ClientError('Invalid channel type'));
+	const allowedChannelTypes = ["text", "voice"];
+	if (allowedChannelTypes.includes(channel_type)) {
+		return handleRequest(api.post<ApiResponse<Channel[]>>(`/api/v1/channels/${categoryId}/channel_name/${name}/${channel_type}`));
+	}
+	return Promise.reject(new ClientError('Invalid channel type'));
 }
 
 export async function renameChannel(channelId: number, name: string): Promise<Channel[]> {
-  return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/rename/${name}`));
+	return handleRequest(api.patch<ApiResponse<Channel[]>>(`/api/v1/channels/${channelId}/rename/${name}`));
 }
 
 export async function renameCategory(channelId: number, name: string): Promise<Category[]> {
-   return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${channelId}/rename/${name}`));
+	return handleRequest(api.patch<ApiResponse<Category[]>>(`/api/v1/categories/${channelId}/rename/${name}`));
 }
 
 export async function getCategoryAccessRoles(categoryId: number): Promise<Role[]> {
-  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`));
+	return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`));
 }
 
 export async function editCategoryPermissions(categoryId: number, rolesWithAccess: number[]): Promise<Role[]> {
-  return handleRequest(api.put<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`, rolesWithAccess));
+	return handleRequest(api.put<ApiResponse<Role[]>>(`/api/v1/categories/${categoryId}/permissions`, rolesWithAccess));
 }
 
 export async function getAllRoles(): Promise<Role[]> {
-  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles`));
+	return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles`));
 }
 
 export async function getEditableRoles(): Promise<Role[]> {
-  return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles/editable`));
+	return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/roles/editable`));
 }
 
 export async function createRole(name: string): Promise<Role[]> {
-  return handleRequest(api.post<ApiResponse<Role[]>>(`/api/v1/roles/${name}`));
+	return handleRequest(api.post<ApiResponse<Role[]>>(`/api/v1/roles/${name}`));
 }
 
 export async function renameRole(roleId: number, name: string): Promise<Role[]> {
-  return handleRequest(api.patch<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}/rename/${name}`));
+	return handleRequest(api.patch<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}/rename/${name}`));
 }
 
 export async function deleteRole(roleId: number): Promise<Role[]> {
-  return handleRequest(api.delete<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}`));
+	return handleRequest(api.delete<ApiResponse<Role[]>>(`/api/v1/roles/${roleId}`));
 }
 
 export async function getUsers(): Promise<User[]> {
-  return handleRequest(api.get<ApiResponse<User[]>>(`/api/v1/users`));
+	return handleRequest(api.get<ApiResponse<User[]>>(`/api/v1/users`));
+}
+
+export async function renameUser(userId: number, name: string): Promise<User[]> {
+	return handleRequest(api.patch<ApiResponse<User[]>>(`/api/v1/users/${userId}/rename/${name}`));
+}
+
+export async function kickUser(userId: number): Promise<User[]> {
+	return handleRequest(api.delete<ApiResponse<User[]>>(`/api/v1/users/${userId}`));
+}
+
+export async function getUserRoles(userId: number): Promise<Role[]> {
+	return handleRequest(api.get<ApiResponse<Role[]>>(`/api/v1/users/${userId}`));
+}
+
+export async function editUserRoles(userId: number, roles: number[]): Promise<User[]> {
+	return handleRequest(api.put<ApiResponse<User[]>>(`/api/v1/users/${userId}/`, roles));
 }
--- frontend/src/pages/Groups.tsx
+++ frontend/src/pages/Groups.tsx
@@ -122,127 +122,127 @@ export default function Groups({ itemsPerPage = ITEMS_PER_PAGE }: { itemsPerPage
     [setRoles]
   );
 
-    const handleRenameRole = useCallback(
-      async (roleId: number, roleName: string) => {
-        setRoles((prevRoles) =>
-          prevRoles.map((role) =>
-            role.id === roleId ? { ...role, name: roleName } : role
-          )
-        );
-        setActionSidebar({ action: null, target: null, item: null });
-        try {
-          const roles = await renameRole(roleId, roleName);
-          setRoles(roles);
-        } catch (error) {
-          toast.error(error.message, {
-            position: "bottom-right",
-            duration: 10000
-          });
-          const fetchedRoles = await getEditableRoles();
-          setRoles(fetchedRoles);
-        }
-      },
-      [setRoles]
-    );
+  const handleRenameRole = useCallback(
+    async (roleId: number, roleName: string) => {
+      setRoles((prevRoles) =>
+        prevRoles.map((role) =>
+          role.id === roleId ? { ...role, name: roleName } : role
+        )
+      );
+      setActionSidebar({ action: null, target: null, item: null });
+      try {
+        const roles = await renameRole(roleId, roleName);
+        setRoles(roles);
+      } catch (error) {
+        toast.error(error.message, {
+          position: "bottom-right",
+          duration: 10000
+        });
+        const fetchedRoles = await getEditableRoles();
+        setRoles(fetchedRoles);
+      }
+    },
+    [setRoles]
+  );
 
-    const handleDeleteRole = useCallback(
-      async (roleId: number) => {
-          setRoles((prevRoles) => prevRoles.filter((role) => role.id !== roleId));
-          setActionSidebar({action: null, target: null, item: null});
-        try {
-          const roles = await deleteRole(roleId);
-          setRoles(roles);
-        } catch (error) {
-          toast.error(error.message, {
-            position: "bottom-right",
-            duration: 10000
-          });
-          const fetchedRoles = await getEditableRoles();
-          setRoles(fetchedRoles);
-        }
-      },
-      [setRoles]
-    );
+  const handleDeleteRole = useCallback(
+    async (roleId: number) => {
+        setRoles((prevRoles) => prevRoles.filter((role) => role.id !== roleId));
+        setActionSidebar({action: null, target: null, item: null});
+      try {
+        const roles = await deleteRole(roleId);
+        setRoles(roles);
+      } catch (error) {
+        toast.error(error.message, {
+          position: "bottom-right",
+          duration: 10000
+        });
+        const fetchedRoles = await getEditableRoles();
+        setRoles(fetchedRoles);
+      }
+    },
+    [setRoles]
+  );
 
 
-    return (
-      <div className="flex w-full h-full p-5">
-        <div className="w-2/3 h-full flex flex-col">
-          {!isLoading && (
-            <div className="mb-5 flex justify-between items-center">
-              <div className="w-full flex flex-row relative">
-                <input
-                  type="text"
-                  placeholder="Пошук за назвою групи..."
-                  className="w-full p-2 rounded bg-[#292B2F] focus:outline-none pr-8"
-                  value={searchTerm}
-                  onChange={handleSearch}
-                />
-                <img
-                  src={SearchIcon}
-                  alt="Пошук"
-                  className="w-5 h-5 absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none"
-                />
-              </div>
-              <div
-                className="flex justify-center p-2 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer w-1/3 ml-5 transition-all duration-300"
-                onClick={() => handleActionTriggered('create', 'role', null)}>
-                <img src={CreateRoleIcon} alt="Створити групу" className="w-5 h-5"/>
-              </div>
+  return (
+    <div className="flex w-full h-full p-5">
+      <div className="w-2/3 h-full flex flex-col">
+        {!isLoading && (
+          <div className="mb-5 flex justify-between items-center">
+            <div className="w-full flex flex-row relative">
+              <input
+                type="text"
+                placeholder="Пошук за назвою групи..."
+                className="w-full p-2 rounded bg-[#292B2F] focus:outline-none pr-8"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+              <img
+                src={SearchIcon}
+                alt="Пошук"
+                className="w-5 h-5 absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none"
+              />
             </div>
-          )}
-
-
-          {isLoading ? (
-            <ComponentLoadingSpinner/>
-          ) : (
-            <div className="flex-grow">
-              {filteredRoles.length === 0 ? (
-                <div className="text-gray-400">Груп немає</div>
-              ) : (
-                <ul className="list-none p-0 space-y-2">
-                  {rolesOnPage.map((role) => (
-                    <li key={role.id}
-                      className="py-2 px-3 rounded bg-[#2f3136] text-white flex justify-between items-center">
-                      {role.name}
-                      <div className="flex items-center space-x-2">
-                        <button onClick={() => handleActionTriggered('rename', 'role', role)}>
-                          <img
-                            src={RenameIcon}
-                            alt="Перейменувати"
-                            className="w-5 h-5 cursor-pointer hover:brightness-200 transition-all duration-300"/>
-                        </button>
-                        <button onClick={() => handleActionTriggered('delete', 'role', role)}>
-                          <img
-                            src={DeleteIcon}
-                            alt="Перейменувати"
-                            className="w-5 h-5 cursor-pointer hover:brightness-200 transition-all duration-300"/>
-                        </button>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              )}
+            <div
+              className="flex justify-center p-2 border-dashed border-gray-500 text-gray-300 hover:border-gray-400 hover:text-gray-100 border rounded cursor-pointer w-1/3 ml-5 transition-all duration-300"
+              onClick={() => handleActionTriggered('create', 'role', null)}>
+              <img src={CreateRoleIcon} alt="Створити групу" className="w-5 h-5"/>
             </div>
-          )}
-
-          {!isLoading && (
-            <PaginationControl currentPage={currentPage} pageCount={pageCount} handlePageChange={handlePageChange}/>
-          )}
-        </div>
-        {actionSidebar.action && actionSidebar.target && (
-          <div className="pl-5 w-1/3">
-            <ActionSidebar
-              action={actionSidebar.action}
-              target={actionSidebar.target}
-              item={actionSidebar.item}
-              onCancel={handleCancelAction}
-              onCreateRole={handleCreateRole}
-              onRenameRole={handleRenameRole}
-              onDeleteRole={handleDeleteRole}
-            />
           </div>
         )}
+
+
+        {isLoading ? (
+          <ComponentLoadingSpinner/>
+        ) : (
+          <div className="flex-grow">
+            {filteredRoles.length === 0 ? (
+              <div className="text-gray-400">Груп немає</div>
+            ) : (
+              <ul className="list-none p-0 space-y-2">
+                {rolesOnPage.map((role) => (
+                  <li key={role.id}
+                    className="py-2 px-3 rounded bg-[#2f3136] text-white flex justify-between items-center">
+                    {role.name}
+                    <div className="flex items-center space-x-2">
+                      <button onClick={() => handleActionTriggered('rename', 'role', role)}>
+                        <img
+                          src={RenameIcon}
+                          alt="Перейменувати"
+                          className="w-5 h-5 cursor-pointer hover:brightness-200 transition-all duration-300"/>
+                      </button>
+                      <button onClick={() => handleActionTriggered('delete', 'role', role)}>
+                        <img
+                          src={DeleteIcon}
+                          alt="Перейменувати"
+                          className="w-5 h-5 cursor-pointer hover:brightness-200 transition-all duration-300"/>
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
+
+        {!isLoading && (
+          <PaginationControl currentPage={currentPage} pageCount={pageCount} handlePageChange={handlePageChange}/>
+        )}
       </div>
-    );
+      {actionSidebar.action && actionSidebar.target && (
+        <div className="pl-5 w-1/3">
+          <ActionSidebar
+            action={actionSidebar.action}
+            target={actionSidebar.target}
+            item={actionSidebar.item}
+            onCancel={handleCancelAction}
+            onCreateRole={handleCreateRole}
+            onRenameRole={handleRenameRole}
+            onDeleteRole={handleDeleteRole}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
