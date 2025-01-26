d13e6126e9b9d43ff99d74b95a997c53e34acdd8
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-26 19:19:10 +0200

    feat: implement logic to redirect regular users without access

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+// src/components/ProtectedRoute.tsx
+import React, { useEffect, useState } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 import { ComponentLoadingSpinner } from '@/components/LoadingSpinner';
+import { getUser } from '@/lib/api';
+import NoAccessPage from '@/pages/NoAccessPage';
 import { Header } from '@/layout/Header';
 import { Sidebar } from '@/layout/Sidebar';
 
@@ -12,13 +15,40 @@ interface ProtectedRouteProps {
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { user, loading } = useAuth();
   const location = useLocation();
+  const [userGroup, setUserGroup] = useState<string | null>(null);
+  const [isLoadingGroup, setIsLoadingGroup] = useState<boolean>(true);
 
-  if (loading) {
+  useEffect(() => {
+    const fetchUserGroup = async () => {
+      if (user?.user_metadata?.provider_id) {
+        try {
+          const fetchedUser = await getUser(user.user_metadata.provider_id);
+          console.log(fetchedUser)
+          setUserGroup(fetchedUser.group || null);
+        } catch (error) {
+          console.error("Error fetching user group:", error);
+          setUserGroup(null);
+        } finally {
+          setIsLoadingGroup(false);
+        }
+      } else {
+        setIsLoadingGroup(false);
+      }
+    };
+
+    fetchUserGroup();
+  }, [user]);
+
+  if (loading || isLoadingGroup) {
     return <ComponentLoadingSpinner />;
   }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (userGroup !== 'staff') {
+    return <NoAccessPage />;
   }
 
   return (
