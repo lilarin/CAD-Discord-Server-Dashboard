741d12f9e61b999f18f8501731bdebe573eeb737
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-26 19:14:33 +0200

    feat: implement page for users without access to the panel

---
+++ frontend/src/pages/NoAccessPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Button} from "@/components/ui/button.tsx";
+import LogOutIcon from "@/assets/icons/logout.svg";
+import {useAuth} from "@/contexts/AuthContext.tsx";
+
+const NoAccessPage: React.FC = () => {
+	const { signOut, loading } = useAuth();
+
+	const handleSignOut = async () => {
+	  await signOut();
+	};
+
+
+  return (
+	  <div className="flex flex-col items-center justify-center h-screen bg-[#36393F] text-white">
+		  <h2 className="text-2xl mb-5">Доступ заборонено</h2>
+		  <p className="mb-5 text-gray-400 text-center">
+			  Вибачте, у вас немає прав доступу до панелі керування.<br/>
+			  Для отримання доступу зверніться до адміністратора, або зайдіть з іншого аккаунту.
+		  </p>
+		  <Button
+				  variant="ghost"
+				  size="default"
+				  className="text-gray-400 hover:text-gray-200 rounded flex items-center gap-2 transition-all duration-300"
+				  onClick={handleSignOut}
+				  disabled={loading}
+			  >
+				  <img src={LogOutIcon} alt="Вийти з системи" className="w-5 h-5 cursor-pointer"/>
+				  {loading ? "Вихід..." : "Вийти з системи"}
+		  </Button>
+	  </div>
+  );
+};
+
+export default NoAccessPage;
