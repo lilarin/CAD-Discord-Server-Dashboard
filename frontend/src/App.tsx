9b5f3d23e6bf9df7b8f1eb4ac9e733d39fbd26bd
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-26 18:25:03 +0200

    feat: add AuthProvider to handle routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Header } from "@/layout/Header";
-import { Sidebar } from "@/layout/Sidebar";
 import Categories from "@/pages/Categories.tsx";
 import Groups from "@/pages/Groups";
 import Users from "@/pages/Users";
@@ -9,28 +7,26 @@ import Events from "@/pages/Events";
 import Archivation from "@/pages/Archivation";
 import Logs from "@/pages/Logs";
 import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from '@/contexts/AuthContext';
+import LoginPage from '@/pages/LoginPage';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Toaster />
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <div className="flex flex-1">
-          <Sidebar />
-          <main className="flex-1 bg-[#36393F]">
-            <Routes>
-              <Route path="/" element={<Navigate to="/disciplines" replace />} />
-              <Route path="/disciplines" element={<Categories />} />
-              <Route path="/groups" element={<Groups />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/archivation" element={<Archivation />} />
-              <Route path="/logs" element={<Logs />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
+      <AuthProvider>
+        <Toaster />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} /> {/* Сторінка логіна без ProtectedRoute */}
+          <Route path="/" element={<ProtectedRoute><Navigate to="/disciplines" replace /></ProtectedRoute> } />
+          <Route path="/disciplines" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
+          <Route path="/groups" element={<ProtectedRoute><Groups /></ProtectedRoute>} />
+          <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
+          <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
+          <Route path="/archivation" element={<ProtectedRoute><Archivation /></ProtectedRoute>} />
+          <Route path="/logs" element={<ProtectedRoute><Logs /></ProtectedRoute>} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
