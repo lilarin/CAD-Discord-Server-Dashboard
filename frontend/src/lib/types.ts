202891e63ad17c713e0693783568efd21031d3c6
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-28 01:23:16 +0200

    feat: implement datatypes for the request body

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -3,16 +3,24 @@ export interface Category {
     name: string;
 }
 
-export interface Role {
-    id: number;
-    name: string;
-}
-
 export interface Channel {
     id: number;
     position: number;
     name: string;
     type: string
+}
+
+export interface Role {
+    id: number;
+    name: string;
+}
+
+export interface RenameRequest {
+  name: string;
+}
+
+export interface ReorderRequest {
+  position: number;
 }
 
 export interface User {
