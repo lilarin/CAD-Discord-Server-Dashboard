442ac6abf204f03ffa35ff47d62d2cd67a69402b
lilarin <80650911+lilarin@users.noreply.github.com>
2025-01-28 20:22:28 +0200

    fix: create interface to store used details to avoid request duplication

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,66 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '@/contexts/AuthContext';
-import { ComponentLoadingSpinner } from '@/components/LoadingSpinner';
-import { getUser } from '@/lib/api';
+import React, {useEffect, useState} from 'react';
+import {Navigate, useLocation} from 'react-router-dom';
+import {useAuth} from '@/contexts/AuthContext';
+import {ComponentLoadingSpinner} from '@/components/LoadingSpinner';
+import {getUser} from '@/lib/api';
 import NoAccessPage from '@/pages/NoAccessPage';
-import { Header } from '@/layout/Header';
-import { Sidebar } from '@/layout/Sidebar';
+import {Header} from '@/layout/Header';
+import {Sidebar} from '@/layout/Sidebar';
+import {User} from '@/lib/types';
 
 interface ProtectedRouteProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { user, loading } = useAuth();
-  const location = useLocation();
-  const [userGroup, setUserGroup] = useState<string | null>(null);
-  const [isLoadingGroup, setIsLoadingGroup] = useState<boolean>(true);
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({children}) => {
+	const {user, loading} = useAuth();
+	const location = useLocation();
+	const [userGroup, setUserGroup] = useState<string | null>(null);
+	const [userDetails, setUserDetails] = useState<User | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchUserGroup = async () => {
-      if (user?.user_metadata?.provider_id) {
-        try {
-          const fetchedUser = await getUser(user.user_metadata.provider_id);
-          console.log(fetchedUser)
-          setUserGroup(fetchedUser.group || null);
-        } catch (error) {
-          console.error("Error fetching user group:", error);
-          setUserGroup(null);
-        } finally {
-          setIsLoadingGroup(false);
-        }
-      } else {
-        setIsLoadingGroup(false);
-      }
-    };
+	useEffect(() => {
+		const fetchUserData = async () => {
+			if (!user?.user_metadata?.provider_id) {
+				setIsLoading(false);
+				return;
+			}
 
-    fetchUserGroup();
-  }, [user]);
+			try {
+				const fetchedUser = await getUser(user.user_metadata.provider_id);
+				setUserGroup(fetchedUser.group || null);
+				setUserDetails(fetchedUser);
+			} catch (error) {
+				console.error("Error fetching user data:", error);
+				setUserGroup(null);
+				setUserDetails(null)
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		setIsLoading(true);
+		fetchUserData();
+	}, [user?.user_metadata?.provider_id]);
 
-  if (loading || isLoadingGroup) {
-    return <ComponentLoadingSpinner />;
-  }
 
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+	if (loading || isLoading) {
+		return <ComponentLoadingSpinner/>;
+	}
 
-  if (userGroup !== 'staff') {
-    return <NoAccessPage />;
-  }
+	if (!user) {
+		return <Navigate to="/login" state={{from: location}} replace/>;
+	}
 
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 bg-[#36393F]">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
+	if (userGroup !== 'staff') {
+		return <NoAccessPage/>;
+	}
+
+	return (
+		<div className="min-h-screen flex flex-col">
+			<Header userDetails={userDetails}/>
+			<div className="flex flex-1">
+				<Sidebar/>
+				<main className="flex-1 bg-[#36393F]">
+					{children}
+				</main>
+			</div>
+		</div>
+	);
 };
 
 export default ProtectedRoute;
--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,89 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { UserCircle } from "lucide-react";
-import { Button } from "@/components/Button.tsx";
+import React from "react";
+import {UserCircle} from "lucide-react";
+import {Button} from "@/components/Button.tsx";
 import LogOutIcon from "@/assets/icons/logout.svg";
-import { useAuth } from "@/contexts/AuthContext";
-import { getUser } from "@/lib/api";
-import { User } from "@/lib/types";
-
-export const Header = () => {
-  const { signOut, loading, user } = useAuth();
-  const [userDetails, setUserDetails] = useState<User | null>(null);
-  const [isLoadingUserDetails, setIsLoadingUserDetails] = useState<boolean>(false);
-  const [isInitiated, setIsInitiated] = useState(false);
-
-  const handleSignOut = async () => {
-    await signOut();
-  };
-
-  useEffect(() => {
-    const fetchUserDetails = async () => {
-      if (user?.user_metadata?.provider_id) {
-        if (!userDetails && !isInitiated) {
-          setIsLoadingUserDetails(true);
-          try {
-            const fetchedUser = await getUser(user.user_metadata.provider_id);
-            setUserDetails(fetchedUser);
-          } catch (error) {
-            console.error("Error fetching user details:", error);
-          } finally {
-            setIsLoadingUserDetails(false);
-            setIsInitiated(true);
-          }
-        }
-      }
-    };
-
-    fetchUserDetails();
-  }, [isInitiated, user, userDetails]);
-
-  const userName = () => {
-    if (userDetails?.name) {
-      return userDetails.name;
-    }
-    return user.user_metadata.full_name;
-  };
-
-  const userAvatarUrl = () => {
-    if (user?.user_metadata?.avatar_url) {
-      return user.user_metadata.avatar_url;
-    }
-    return null;
-  };
+import {useAuth} from "@/contexts/AuthContext";
+import {User} from "@/lib/types";
 
 
-  return (
-    <header className="bg-[#292B2F]">
-      <div className="flex h-14 items-center justify-between">
-        <div className="w-1/6 flex justify-center">
-          <div className="flex items-center gap-3">
-            {userAvatarUrl() ? (
-              <img
-                src={userAvatarUrl()}
-                alt="Аватар користувача"
-                className="rounded-full h-8 w-8"
-              />
-            ) : (
-              <UserCircle className="h-8 w-8 text-gray-400" />
-            )}
-            <div className="text-sm">
-              <p className="font-medium text-gray-200">{isLoadingUserDetails ? "Завантаження..." : userName()}</p>
-            </div>
-          </div>
-        </div>
-        <div className="pr-2">
-          <Button
-            variant="ghost"
-            size="default"
-            className="text-gray-400 hover:text-gray-200 rounded flex items-center gap-2 transition-all duration-300"
-            onClick={handleSignOut}
-            disabled={loading}
-          >
-            <img src={LogOutIcon} alt="Вийти з системи" className="w-5 h-5 cursor-pointer" />
-            {loading ? "Вихід..." : "Вийти з системи"}
-          </Button>
-        </div>
-      </div>
-    </header>
-  );
+interface HeaderProps {
+	userDetails: User | null;
+}
+
+export const Header: React.FC<HeaderProps> = ({userDetails}) => {
+	const {signOut, loading, user} = useAuth();
+
+	const handleSignOut = async () => {
+		await signOut();
+	};
+
+
+	const userName = () => {
+		if (userDetails?.name) {
+			return userDetails.name;
+		}
+		return user?.user_metadata?.full_name || 'User';
+	};
+
+	const userAvatarUrl = () => {
+		if (user?.user_metadata?.avatar_url) {
+			return user.user_metadata.avatar_url;
+		}
+		return null;
+	};
+
+	return (
+		<header className="bg-[#292B2F]">
+			<div className="flex h-14 items-center justify-between">
+				<div className="w-1/6 flex justify-center">
+					<div className="flex items-center gap-3">
+						{userAvatarUrl() ? (
+							<img
+								src={userAvatarUrl()}
+								alt="Аватар користувача"
+								className="rounded-full h-8 w-8"
+							/>
+						) : (
+							<UserCircle className="h-8 w-8 text-gray-400"/>
+						)}
+						<div className="text-sm">
+							<p className="font-medium text-gray-200">{!userDetails ? "Завантаження..." : userName()}</p>
+						</div>
+					</div>
+				</div>
+				<div className="pr-2">
+					<Button
+						variant="ghost"
+						size="default"
+						className="text-gray-400 hover:text-gray-200 rounded flex items-center gap-2 transition-all duration-300"
+						onClick={handleSignOut}
+						disabled={loading}
+					>
+						<img src={LogOutIcon} alt="Вийти з системи" className="w-5 h-5 cursor-pointer"/>
+						{loading ? "Вихід..." : "Вийти з системи"}
+					</Button>
+				</div>
+			</div>
+		</header>
+	);
 };
