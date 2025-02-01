f0a20a5db1fd635fce069c378d036dfe088a466b
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-01 19:27:59 +0200

    feat: implement obtaining request log message

--- backend/middlewares/authorization.py
+++ backend/middlewares/authorization.py
@@ -1,22 +1,20 @@
 from typing import Optional
+from urllib.parse import unquote
 
 import disnake
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
-from supabase import create_client, Client
 
-from backend.config import config
 from backend.middlewares.schemas import ResponseWrapper
 from backend.services.fetch import fetch_user
+from backend.services.supabase_client import supabase, save_log_to_supabase
 from backend.services.utils import get_user_group
-
-supabase: Client = create_client(config.supabase_url, config.supabase_key)
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        if request.method == "OPTIONS" or request.url.path in ["/", "/docs", "/openapi.json"]:
+        if request.method == "OPTIONS" or request.url.path in ["/", "/docs", "/openapi.json", "/api/v1/logs"]:
             response = await call_next(request)
             return response
 
@@ -31,8 +29,12 @@ class AuthMiddleware(BaseHTTPMiddleware):
             if user_data.user and user_data.user.user_metadata:
                 user = await fetch_user(user_data.user.user_metadata["provider_id"])
                 user_group = await get_user_group(user)
-                if user_group == "staff":
+                if user_group:
                     request.state.user = user_data.user
+                    action = request.headers.get("X-Request-Source-Method")
+                    if action:
+                        action_text = unquote(action)
+                        await save_log_to_supabase(user, action_text)
                     response = await call_next(request)
                     return response
                 else:
