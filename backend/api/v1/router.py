2af7681704711cb4d41ebc3af62e9ed93d885969
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-16 01:38:51 +0200

    feat: add Bearer dependencies for the API endpoints

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from backend.api.v1.categories import router as categories_router
 from backend.api.v1.channels import router as channels_router
@@ -6,7 +7,7 @@ from backend.api.v1.logs import router as logs_router
 from backend.api.v1.roles import router as roles_router
 from backend.api.v1.users import router as users_router
 
-router = APIRouter(prefix="/v1")
+router = APIRouter(prefix="/v1", dependencies=[Depends(HTTPBearer())],)
 
 router.include_router(categories_router, tags=["Categories"])
 router.include_router(channels_router, tags=["Channels"])
