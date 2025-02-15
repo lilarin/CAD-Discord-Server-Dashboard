13ade0a74cafef443d129e326d126f9951b20d0c
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-15 18:10:04 +0200

    feat: add supabase decorator to service functions

--- backend/services/supabase_client.py
+++ backend/services/supabase_client.py
@@ -6,11 +6,13 @@ from fastapi import HTTPException
 from supabase import create_client, Client
 
 from backend.config import config
+from backend.middlewares.supabase import logs_table_exists
 from backend.schemas import LogSchema
 
 supabase: Client = create_client(config.supabase_url, config.supabase_key)
 
 
+@logs_table_exists
 async def save_log_to_supabase(user: Member, action: str) -> None:
     try:
         name = user.display_name
@@ -29,6 +31,7 @@ async def save_log_to_supabase(user: Member, action: str) -> None:
         raise HTTPException(detail=f"Error during Supabase logging: {e}", status_code=500)
 
 
+@logs_table_exists
 async def read_logs_from_supabase() -> list[LogSchema]:
     try:
         response = (
