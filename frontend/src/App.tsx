988d6be84c2b10fba09618e16e1a9978c9198f3a
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-16 19:57:26 +0200

    refactor: update routes to include homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Categories from "@/pages/Categories.tsx";
+import React, {useEffect, useState} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Categories from "@/pages/Disciplines.tsx";
 import Groups from "@/pages/Groups";
 import Users from "@/pages/Users";
 import Events from "@/pages/Events";
 import Logs from "@/pages/Logs";
-import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from '@/contexts/AuthContext';
+import {Toaster} from 'react-hot-toast';
+import {AuthProvider} from '@/contexts/AuthContext';
 import LoginPage from '@/pages/LoginPage';
 import ProtectedRoute from '@/components/ProtectedRoute';
 import NotFoundPage from "@/pages/NotFoundPage";
 import PCViewOnlyPage from "@/components/PCViewOnly";
+import HomePage from "@/pages/HomePage";
 
 const App = () => {
-  const [isMobile, setIsMobile] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    const mobileKeywords = ['android', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone'];
+	useEffect(() => {
+		const userAgent = navigator.userAgent.toLowerCase();
+		const mobileKeywords = ['android', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone'];
 
-    const isMobileDevice = mobileKeywords.some(keyword => userAgent.includes(keyword));
-    setIsMobile(isMobileDevice);
-  }, []);
+		const isMobileDevice = mobileKeywords.some(keyword => userAgent.includes(keyword));
+		setIsMobile(isMobileDevice);
+	}, []);
 
-  if (isMobile) {
-    return <PCViewOnlyPage />;
-  }
+	if (isMobile) {
+		return <PCViewOnlyPage/>;
+	}
 
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Toaster />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<ProtectedRoute><Navigate to="/disciplines" replace /></ProtectedRoute> } />
-          <Route path="/disciplines" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
-          <Route path="/groups" element={<ProtectedRoute><Groups /></ProtectedRoute>} />
-          <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
-          <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
-          <Route path="/logs" element={<ProtectedRoute><Logs /></ProtectedRoute>} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<AuthProvider>
+				<Toaster/>
+				<Routes>
+					<Route path="/login" element={<LoginPage/>}/>
+					<Route path="/" element={<ProtectedRoute><HomePage/></ProtectedRoute>}/>
+					<Route path="/categories" element={<ProtectedRoute><Categories/></ProtectedRoute>}/>
+					<Route path="/groups" element={<ProtectedRoute><Groups/></ProtectedRoute>}/>
+					<Route path="/users" element={<ProtectedRoute><Users/></ProtectedRoute>}/>
+					<Route path="/events" element={<ProtectedRoute><Events/></ProtectedRoute>}/>
+					<Route path="/logs" element={<ProtectedRoute><Logs/></ProtectedRoute>}/>
+					<Route path="*" element={<NotFoundPage/>}/>
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
