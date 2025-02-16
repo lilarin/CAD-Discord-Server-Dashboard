3c93400a4015d39807f51b0d47fb6e1bdc24384e
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-16 21:04:12 +0200

    feat: add new .env variables to the config

--- backend/config.py
+++ backend/config.py
@@ -9,6 +9,11 @@ load_dotenv()
 class Config:
     def __init__(self):
         self.discord_bot_token = self._get_env_variable("DISCORD_BOT_TOKEN")
+        self.administrator_role_id = int(self._get_env_variable("ADMINISTRATOR_ROLE_ID"))
+        self.teacher_role_id = int(self._get_env_variable("TEACHER_ROLE_ID"))
+        self.student_role_id = int(self._get_env_variable("STUDENT_ROLE_ID"))
+        self.guild_id = int(self._get_env_variable("GUILD_ID"))
+
         self.supabase_direct_url = self._get_env_variable("SUPABASE_DIRECT_URL")
         self.supabase_url = self._get_env_variable("VITE_SUPABASE_URL")
         self.supabase_key = self._get_env_variable("VITE_SUPABASE_ANON_KEY")
