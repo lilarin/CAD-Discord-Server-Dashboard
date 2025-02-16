e94cf2e32b63c631549a5fa4154b01c558a37cf6
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-16 02:06:05 +0200

    feat: add new router for events

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -3,14 +3,16 @@ from fastapi.security import HTTPBearer
 
 from backend.api.v1.categories import router as categories_router
 from backend.api.v1.channels import router as channels_router
+from backend.api.v1.events import router as events_router
 from backend.api.v1.logs import router as logs_router
 from backend.api.v1.roles import router as roles_router
 from backend.api.v1.users import router as users_router
 
-router = APIRouter(prefix="/v1", dependencies=[Depends(HTTPBearer())],)
+router = APIRouter(prefix="/v1", dependencies=[Depends(HTTPBearer())])
 
 router.include_router(categories_router, tags=["Categories"])
 router.include_router(channels_router, tags=["Channels"])
 router.include_router(users_router, tags=["Users"])
 router.include_router(roles_router, tags=["Roles"])
 router.include_router(logs_router, tags=["Logs"])
+router.include_router(events_router, tags=["Events"])
