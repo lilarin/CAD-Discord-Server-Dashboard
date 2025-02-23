fed79a7d4b5195d2a2305529124fdce1266125a0
lilarin <80650911+lilarin@users.noreply.github.com>
2025-02-23 17:30:00 +0200

    refactor: improve i18n config file to return hot-reload and use local locales

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,21 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+
+import en from '@/locales/en.json';
+import uk from '@/locales/uk.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  uk: {
+    translation: uk,
+  },
+};
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: 'uk',
-    debug: false,
-    interpolation: {
-      escapeValue: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}.json',
-    },
   });
 
 export default i18n;
