995195286b58d5b0b7bb484707c142965ae1fea3
lilarin <80650911+lilarin@users.noreply.github.com>
2025-03-01 23:51:33 +0200

    feat: implement methods to manage registration flow

--- backend/services/users_registration.py
+++ backend/services/users_registration.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 
 import disnake
@@ -13,7 +14,7 @@ class UserRegistrationData:
     role: disnake.Role = None
 
 
-async def get_user_info(interaction: disnake.MessageInteraction):
+async def get_registration_user_info(interaction: disnake.MessageInteraction) -> UserRegistrationData | None:
     user_info = USER_REGISTER_DATA.get(interaction.user.id)
 
     if user_info:
@@ -21,5 +22,24 @@ async def get_user_info(interaction: disnake.MessageInteraction):
 
     await send_ephemeral_response(
         interaction,
-        "Будь ласка, розпочніть реєстрацію заново, стара вже форма не дійсна"
+        "Будь ласка, розпочніть реєстрацію заново, стара форма вже не дійсна"
     )
+
+
+async def add_registration_user(user_id: int, name: str) -> None:
+    USER_REGISTER_DATA[user_id] = UserRegistrationData(full_name=name)
+
+
+async def remove_registration_user(user_id: int) -> None:
+    USER_REGISTER_DATA.pop(user_id)
+
+
+async def check_user_name(interaction: disnake.ModalInteraction, name: str) -> bool:
+    match_patter = r"[А-Яа-яЄєІіЇїҐґ'’\s-]+"
+    if not re.fullmatch(match_patter, name) or " " not in name:
+        await send_ephemeral_response(
+            interaction,
+            "Введені дані повинні містити тільки кирилицю, пробіли або апостроф"
+        )
+        return False
+    return True
