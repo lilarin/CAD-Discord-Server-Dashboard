80249e619d3ce164f9833955c11e84e176fbf9b4
lilarin <80650911+lilarin@users.noreply.github.com>
2025-03-12 01:42:58 +0200

    feat: implement template settings page

---
+++ frontend/src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useTranslation } from "react-i18next";
+
+const SettingsPage = () => {
+	const { t } = useTranslation();
+
+	return (
+		<div className="container mx-auto p-5 ">
+
+		</div>
+	);
+};
+
+export default SettingsPage;
