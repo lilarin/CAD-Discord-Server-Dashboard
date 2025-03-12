b78f0b56547fbbe853c12e8ead571de291fdce9c
lilarin <80650911+lilarin@users.noreply.github.com>
2025-03-12 21:36:27 +0200

    fix: incorrect endpoint function name and missing disnake errors handling

--- backend/api/v1/queues.py
+++ backend/api/v1/queues.py
@@ -1,3 +1,4 @@
+import disnake
 from fastapi import APIRouter, HTTPException, Body
 
 from backend.middlewares.uniform_response import uniform_response_middleware
@@ -9,10 +10,12 @@ router = APIRouter()
 
 @router.post("/queue")
 @uniform_response_middleware
-async def get_logs(request_body: QueueRequestBody = Body(...)):
+async def create_queue(request_body: QueueRequestBody = Body(...)):
     try:
         await create_queue_message(
             request_body.channel_id, request_body.title, request_body.event_time
         )
+    except disnake.errors.HTTPException as exception:
+        raise HTTPException(status_code=exception.status, detail=str(exception.text))
     except Exception as exception:
         raise HTTPException(status_code=500, detail=str(exception))
